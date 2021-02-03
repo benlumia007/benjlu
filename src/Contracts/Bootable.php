8351dd273ad7db074ac0ed2738e7e628f888f37e
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-02-02 18:49:52 -0800

    initial commit

---
+++ src/Contracts/Bootable.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Bootable interface.
+ *
+ * Defines the contract that bootable classes should utilize. Bootable classes
+ * should have a `boot()` method with the singular purpose of "booting" the
+ * action and filter hooks for that class. This keeps action/filter calls out of
+ * the class constructor. Most bootable classes are meant to be single-instance
+ * classes that get loaded once per page request.
+ *
+ * @package   Mint
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright Copyright (c) 2021, Benjamin Lu
+ * @link      https://getbenonit.com
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+namespace Mint\Contracts;
+
+/**
+ * Bootable interface.
+ *
+ * @since  5.0.0
+ * @access public
+ */
+interface Bootable {
+
+	/**
+	 * Boots the class by running `add_action()` and `add_filter()` calls.
+	 *
+	 * @since  5.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function boot();
+}
