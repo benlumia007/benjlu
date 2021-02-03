bc95c884aadc60fc43e68338147c3689a705a5d7
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-02-02 20:54:53 -0800

    change namespace

--- src/Contracts/Bootable.php
+++ src/Contracts/Bootable.php
@@ -8,19 +8,19 @@
  * the class constructor. Most bootable classes are meant to be single-instance
  * classes that get loaded once per page request.
  *
- * @package   Mint
+ * @package   Merlot
  * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright Copyright (c) 2021, Benjamin Lu
- * @link      https://getbenonit.com
+ * @copyright Copyright 2021. Benjamin Lu
+ * @link      https://github.com/benlumia007/benjlu
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-namespace Mint\Contracts;
+namespace Merlot\Contracts;
 
 /**
  * Bootable interface.
  *
- * @since  5.0.0
+ * @since  1.0.0
  * @access public
  */
 interface Bootable {
@@ -28,7 +28,7 @@ interface Bootable {
 	/**
 	 * Boots the class by running `add_action()` and `add_filter()` calls.
 	 *
-	 * @since  5.0.0
+	 * @since  1.0.0
 	 * @access public
 	 * @return void
 	 */
