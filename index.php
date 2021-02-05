39f2f595514cbabadafa73f04c0b9eda6ef0a1a8
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-02-05 01:55:07 -0800

    updated file naming convention

--- index.php
+++ index.php
@@ -1,83 +1,62 @@
 <?php
+/**
+ * Benjlu ( functions.php )
+ *
+ * @package   Benjlu
+ * @copyright Copyright (C) 2021. Benjamin Lu
+ * @license   GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://getbenonit.com )
+ */
 
 require_once( 'vendor/autoload.php' );
 
+define( 'BENJLU_DIR', __DIR__ );
 
-/* === Application === */
-
-date_default_timezone_set( 'America/Chicago' );
-
-define( 'NOVA_DIR', __DIR__ );
-
-$merlot = new \Merlot\Core\Application();
+$benjlu = new Benlumia007\Alembic\Core\Framework();
 
 $config = require_once( 'config.php' );
 
-$merlot->instance( 'path', NOVA_DIR );
-$merlot->instance( 'uri', $config['uri'] );
-$merlot->instance( 'uri/relative', parse_url( $merlot->uri, PHP_URL_PATH ) );
-$merlot->instance( 'routes', new \Merlot\Routing\Routes() );
-$merlot->singleton( 'cache', function() {
-	return new \Merlot\Tools\Collection();
-} );
-$merlot->instance( 'config/entry', require_once( $merlot->path . '/config/entry.php' ) );
-$merlot->instance( 'path/content', 'user/content' );
-$merlot->singleton( 'template/engine', \Merlot\Template\Engine\Engine::class );
-$merlot->singleton( 'content/types', function() {
-	return new \Merlot\Entry\Types();
-} );
 
-$merlot->instance( 'cache/meta', [ 'date', 'category', 'era', 'slug' ] );
 
-$merlot->singleton( 'mix', function( $app ) {
+$benjlu->instance( 'path', BENJLU_DIR );
+$benjlu->instance( 'uri', $config['uri'] );
+$benjlu->instance( 'uri/relative', parse_url( $benjlu->uri, PHP_URL_PATH ) );
+$benjlu->instance( 'routes', new Benlumia007\Alembic\Routing\Routes() );
+$benjlu->singleton( 'cache', function() { return new Benlumia007\Alembic\Tools\Collection(); } );
+$benjlu->instance( 'config/entry', require_once( $benjlu->path . '/app/functions-entry.php' ) );
+$benjlu->instance( 'path/content', 'user/content' );
+$benjlu->singleton( 'template/engine', Benlumia007\Alembic\Template\Engine\Engine::class );
+$benjlu->singleton( 'content/types', function() { return new Benlumia007\Alembic\Entry\Types(); } );
+$benjlu->instance( 'cache/meta', [ 'date', 'category', 'era', 'slug' ] );
+$benjlu->singleton( 'mix', function( $app ) { $file = "{$app->path}/public/mix-manifest.json"; return file_exists( $file ) ? json_decode( file_get_contents( $file ), true ) : null; } );
+$benjlu->singleton( 'request', Benlumia007\Alembic\Http\Request::class );
+$benjlu->proxy( Benlumia007\Alembic\Proxies\Engine::class, 'Benlumia007\Alembic\Engine' );
+$benjlu->proxy( Benlumia007\Alembic\Proxies\ContentTypes::class, 'Benlumia007\Alembic\ContentTypes' );
+$benjlu->boot();
 
-	$file = "{$app->path}/public/mix-manifest.json";
+$benjlu->routes->get( 'feed', Benlumia007\Alembic\Controllers\Feed::class, 'top' );
+$benjlu->routes->get( 'page/{number}', Benlumia007\Alembic\Controllers\Home::class, 'top' );
 
-	return file_exists( $file ) ? json_decode( file_get_contents( $file ), true ) : null;
-} );
-
-$merlot->singleton( 'request', \Merlot\Http\Request::class );
-
-$merlot->proxy( \Merlot\Proxies\Engine::class, '\Merlot\Engine' );
-$merlot->proxy( \Merlot\Proxies\ContentTypes::class, '\Merlot\ContentTypes' );
-
-$merlot->boot();
-
-/* === Routes === */
-
-$merlot->routes->get( 'feed', \Merlot\Controllers\Feed::class, 'top' );
-
-$merlot->routes->get( 'page/{number}', \Merlot\Controllers\Home::class, 'top' );
-
-\Merlot\ContentTypes::add( 'post', new \Merlot\Entry\Types\Post( $merlot->routes ) );
-
-\Merlot\ContentTypes::add( 'category', new \Merlot\Entry\Types\Category( $merlot->routes ) );
-
-\Merlot\ContentTypes::add( 'era', new \Merlot\Entry\Types\Era( $merlot->routes ) );
-
-\Merlot\ContentTypes::add( 'author', new \Merlot\Entry\Types\Author( $merlot->routes ) );
-
-\Merlot\ContentTypes::add( 'literature', new \Merlot\Entry\Types\Literature( $merlot->routes ) );
-
-\Merlot\ContentTypes::add( 'page', new \Merlot\Entry\Types\Page( $merlot->routes ) );
-
-\Merlot\ContentTypes::registerRoutes();
-
-$merlot->routes->get( '/', \Merlot\Controllers\Home::class );
-
-/* === Launch === */
+Benlumia007\Alembic\ContentTypes::add( 'post', new Benlumia007\Alembic\Entry\Types\Post( $benjlu->routes ) );
+Benlumia007\Alembic\ContentTypes::add( 'category', new Benlumia007\Alembic\Entry\Types\Category( $benjlu->routes ) );
+Benlumia007\Alembic\ContentTypes::add( 'era', new Benlumia007\Alembic\Entry\Types\Era( $benjlu->routes ) );
+Benlumia007\Alembic\ContentTypes::add( 'author', new Benlumia007\Alembic\Entry\Types\Author( $benjlu->routes ) );
+Benlumia007\Alembic\ContentTypes::add( 'literature', new Benlumia007\Alembic\Entry\Types\Literature( $benjlu->routes ) );
+Benlumia007\Alembic\ContentTypes::add( 'page', new Benlumia007\Alembic\Entry\Types\Page( $benjlu->routes ) );
+Benlumia007\Alembic\ContentTypes::registerRoutes();
+$benjlu->routes->get( '/', Benlumia007\Alembic\Controllers\Home::class );
 
 if ( isset( $_GET['bust-cache'] ) ) {
 
 	if ( 'all' === $_GET['bust-cache'] ) {
 
-		$files = glob( $merlot->resolve( 'path' ) . "/user/cache/content/*.json" );
+		$files = glob( $benjlu->resolve( 'path' ) . "/user/cache/content/*.json" );
 
 		foreach ( $files as $filename ) {
 			unlink( $filename );
 		}
 	} else {
-		$path = $merlot->resolve( 'path' ) . '/user/cache';
+		$path = $benjlu->resolve( 'path' ) . '/user/cache';
 
 		$name = preg_replace( '/[^A-Za-z0-9\/_-]/i', '', $_GET['bust-cache'] );
 
@@ -97,4 +76,4 @@ if ( ini_get( 'output_handler' ) !== 'ob_gzhandler' ) {
 }
 
 // Launch the current controller.
-$current = $merlot->routes->current();
+$current = $benjlu->routes->current();
