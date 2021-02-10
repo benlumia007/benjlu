476a84a7298edf5b3486186e43ceae6b59606d9e
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-02-10 13:16:31 -0800

    updated

--- app/functions-setup.php
+++ app/functions-setup.php
@@ -1,42 +1,21 @@
 <?php
 
-use Benlumia007\Alembic\App;
-
 function site_title() {
-	if ( ! file_exists( 'user/content/_data/config.yml' ) ) {
-		return;
-	}
-	$data = App::resolve( 'yaml' )->load( ( 'user/content/_data/config.yml' ) );
+	$data = Benlumia007\Alembic\Config\File::get_instance()->get_data();
 
-	if ( $data ) {
-		$title = $data['title'];
-		echo strip_tags( $title );
-	}
+	$title = $data['title'];
+	echo strip_tags( $title );
 }
 
 function site_tagline() {
-	if ( ! file_exists( 'user/content/_data/config.yml' ) ) {
-		return;
-	}
-
-	$data = App::resolve( 'yaml' )->load( ( 'user/content/_data/config.yml' ) );
-
-	if ($data) {
-		$title = $data['tagline'];
-		echo strip_tags( $title );
-	}
+	$data = Benlumia007\Alembic\Config\File::get_instance()->get_data();
+	$title = $data['tagline'];
+	echo strip_tags( $title );
 }
 
 function primary_navigation() {
-	if ( ! file_exists( 'user/content/_data/config.yml' ) ) {
-		return;
-	}
-
-	$data = App::resolve( 'yaml' )->load( ( 'user/content/_data/config.yml' ) );
-
-	if ( $data ) {
-		foreach ($data['primary'] as $name => $title ) { ?>
-			<li class="menu-item"><a href="<?= e( uri( $title['url'] ) ) ?>"><?= e( $title['title'] ); ?></a></li>
-		<?php }
-	}
+	$data = Benlumia007\Alembic\Config\File::get_instance()->get_data();
+	foreach ($data['primary'] as $name => $title ) { ?>
+		<li class="menu-item"><a href="<?= e( uri( $title['url'] ) ) ?>"><?= e( $title['title'] ); ?></a></li>
+	<?php }
 }
