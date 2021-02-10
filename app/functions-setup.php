4e12d102886ca42723b3ea74a0229a235540f643
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-02-10 08:30:17 -0800

    updated

--- app/functions-setup.php
+++ app/functions-setup.php
@@ -1,20 +1,42 @@
 <?php
 
+use Benlumia007\Alembic\App;
+
 function site_title() {
-	$data = Benlumia007\Alembic\App::resolve( 'yaml' )->load( ( file_exists( 'user/content/_data/config.yml' ) ) ? 'user/content/_data/config.yml' : '' );
-	$title = $data['title'];
-	echo strip_tags( $title );
+	if ( ! file_exists( 'user/content/_data/config.yml' ) ) {
+		return;
+	}
+	$data = App::resolve( 'yaml' )->load( ( 'user/content/_data/config.yml' ) );
+
+	if ( $data ) {
+		$title = $data['title'];
+		echo strip_tags( $title );
+	}
 }
 
 function site_tagline() {
-	$data = Benlumia007\Alembic\App::resolve( 'yaml' )->load( ( file_exists( 'user/content/_data/config.yml' ) ) ? 'user/content/_data/config.yml' : '' );
-	$title = $data['tagline'];
-	echo strip_tags( $title );
+	if ( ! file_exists( 'user/content/_data/config.yml' ) ) {
+		return;
+	}
+
+	$data = App::resolve( 'yaml' )->load( ( 'user/content/_data/config.yml' ) );
+
+	if ($data) {
+		$title = $data['tagline'];
+		echo strip_tags( $title );
+	}
 }
 
 function primary_navigation() {
-	$data = Benlumia007\Alembic\App::resolve( 'yaml' )->load( ( file_exists( 'user/content/_data/config.yml' ) ) ? 'user/content/_data/config.yml' : '' );
-	foreach ($data['primary'] as $name => $title ) { ?>
-		<li class="menu-item"><a href="<?= e( uri( $title['url'] ) ) ?>"><?= e( $title['title'] ); ?></a></li>
-	<?php }
+	if ( ! file_exists( 'user/content/_data/config.yml' ) ) {
+		return;
+	}
+
+	$data = App::resolve( 'yaml' )->load( ( 'user/content/_data/config.yml' ) );
+
+	if ( $data ) {
+		foreach ($data['primary'] as $name => $title ) { ?>
+			<li class="menu-item"><a href="<?= e( uri( $title['url'] ) ) ?>"><?= e( $title['title'] ); ?></a></li>
+		<?php }
+	}
 }
