ab2aa0c705e1e489d269a83f886c9f7234b4fa96
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-02-12 14:58:42 -0800

    updated

--- app/functions-setup.php
+++ app/functions-setup.php
@@ -4,13 +4,13 @@ function site_title() {
 	$data = Benlumia007\Alembic\Config\File::get_instance()->get_data();
 
 	$title = $data['title'];
-	echo strip_tags( $title );
+	return strip_tags( $title );
 }
 
 function site_tagline() {
 	$data = Benlumia007\Alembic\Config\File::get_instance()->get_data();
 	$title = $data['tagline'];
-	echo strip_tags( $title );
+	return strip_tags( $title );
 }
 
 function primary_navigation() {
